5a135bffab788bdffe8b8afdf97c6b9e3fc9566a
krachol <mateusz.krasicki@gmail.com>
2018-06-13 00:38:12 +0200

    Add leaderboard stub

---
+++ DFDSBetting/Controllers/LeaderboardController.cs
@@ -0,0 +1,28 @@
+﻿using DFDSBetting.Models;
+using DFDSBetting.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DFDSBetting.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private ApplicationDbContext _context;
+        private LeaderboardService _leaderboardService;
+
+        public LeaderboardController()
+        {
+            _context = new ApplicationDbContext();
+            _leaderboardService = new LeaderboardService(_context);
+        }
+
+        // GET: Leaderboard
+        public async Task<ActionResult> Index()
+        {
+            List<UsersWithPointsViewModel> viewModel = await _leaderboardService.GetUsersWithPointsAsync();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ DFDSBetting/Services/LeaderboardService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using DFDSBetting.Models;
+
+namespace DFDSBetting.Services
+{
+    internal class LeaderboardService
+    {
+        private ApplicationDbContext _context;
+
+        public LeaderboardService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        internal async Task<List<UsersWithPointsViewModel>> GetUsersWithPointsAsync()
+        {
+            return await _context.Users.Select(u => new UsersWithPointsViewModel
+            {
+                UserName = u.UserName,
+                Points = 0
+            }).ToListAsync();
+        }
+    }
+}
